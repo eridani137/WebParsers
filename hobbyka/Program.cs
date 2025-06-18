28ee977f88da93888f002566ada8c524bcc71ed6
Eridani 137 <tsuuzetsu@gmail.com>
2025-06-18 15:46:17 +0300

    📦 ci: Add .gitignore, solution, and project files

---
+++ hobbyka/Program.cs
@@ -0,0 +1,25 @@
+﻿using Drv;
+using Drv.ChrDrvSettings;
+using Serilog;
+using Serilog.Events;
+
+const string outputTemplate = "[{Timestamp:yyyy-MM-dd HH:mm:ss}] [{Level:u3}] {Message:lj}{NewLine}{Exception}";
+var logsPath = Path.Combine("logs");
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .Enrich.FromLogContext()
+    .WriteTo.Console(outputTemplate: outputTemplate)
+    .WriteTo.File($"{logsPath}/.log", rollingInterval: RollingInterval.Day, outputTemplate: outputTemplate, restrictedToMinimumLevel: LogEventLevel.Error)
+    .CreateLogger();
+
+var drvSettings = new ChrDrvSettingsWithAutoDriver()
+{
+    ChromeDir = @"D:\Chrome",
+    UsernameDir = "NewUser"
+};
+using var drv = await ChrDrvFactory.Create(drvSettings);
+
+drv.Navigate().GoToUrl("https://nowsecure.nl");
+
+Console.ReadKey(true);
