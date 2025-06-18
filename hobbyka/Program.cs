b98e6db55d7fca6a662dd5d1f631a42c914feb91
Eridani 137 <tsuuzetsu@gmail.com>
2025-06-18 16:50:17 +0300

    📝 chore(hobbyka/Program.cs): Add MongoDB client and database configuration

--- hobbyka/Program.cs
+++ hobbyka/Program.cs
@@ -1,10 +1,35 @@
 ﻿using Drv;
+using MongoDB.Bson;
+using MongoDB.Driver;
 using Shared;
 using Spectre.Console;
 
-using var drv = await ChrDrvFactory.Create(Configuration.DrvSettings);
+var categories = new Dictionary<string, string>()
+{
+    ["скамейки"] = "https://hobbyka.ru/catalog/skameyki/"
+};
+var (currentName, currentRootUrl) = categories.ElementAt(0);
 
-drv.Navigate().GoToUrl("https://nowsecure.nl");
+var grid = new Grid();
+grid.AddColumn(new GridColumn());
+grid.AddRow(new Markup($"Текущая категория: {currentName}".MarkupPrimary()));
+grid.AddRow(new Markup($"Текущий URL: {currentRootUrl}".MarkupPrimary()));
+var panel = new Panel(grid)
+    .BorderColor(Color.Yellow)
+    .Border(SpectreConfig.BoxBorder);
+panel.Width = AnsiConsole.Profile.Width;
+AnsiConsole.Write(panel);
+
+var client = new MongoClient("mongodb://eridani:qwerty@localhost:27017/");
+var database = client.GetDatabase("hobbyka");
+var collection = database.GetCollection<BsonDocument>(currentName);
+
+var urls = await File.ReadAllLinesAsync($"{currentName}.txt");
+AnsiConsole.MarkupLine($"Прочитано {urls.Length} строк".MarkupSecondary());
+
+using var drv = await ChrDrvFactory.Create(Configuration.DrvSettings);
+drv.Navigate().GoToUrl(currentRootUrl);
+
 
 AnsiConsole.MarkupLine("Нажмите любую клавишу для выхода...".MarkupSecondary());
 Console.ReadKey(true);
