2538427c0119374b7750fa489c6c23cc76a3912d
Eridani 137 <tsuuzetsu@gmail.com>
2025-06-23 20:05:08 +0300

    ✨ refactor(ActivityChecker/Services/CheckerService.cs): simplify VK parsing logic and handle empty results

--- ActivityChecker/Services/CheckerService.cs
+++ ActivityChecker/Services/CheckerService.cs
@@ -14,33 +14,36 @@ public class CheckerService(ChrDrvSettingsWithoutDriver drvSettings, ParserFacto
     {
         const string results = "results";
         Directory.CreateDirectory(results);
-        
+
         var input = new PathUserInput();
         var lines = (await File.ReadAllLinesAsync(input.Path.Trim('"'))).ToImmutableList();
-        
-        AnsiConsole.MarkupLine($"Прочитано {lines.Count} строк".Highlight(SpectreConfig.Fuchsia, SpectreConfig.Aquamarine));
 
-        if (type == typeof(VkParser))
+        AnsiConsole.MarkupLine(
+            $"Прочитано {lines.Count} строк".Highlight(SpectreConfig.Fuchsia, SpectreConfig.Aquamarine));
+
+        var parser = parserFactory.GetParser(lines.First());
+        if (parser is null) throw new ApplicationException("parser not found");
+
+        var result = await parser.GetViewCount(lines);
+
+        if (result.Count > 0)
         {
-            var parser = parserFactory.GetParser(lines.First());
-            if (parser is not null)
-            {
-                var result = await parser.GetViewCount(lines);
+            AnsiConsole.MarkupLine("Все ссылки обработаны".MarkupAqua());
+            
+            await exporter.Export(Path.Join(results, $"VK_{DateTime.Now:yyyy-MM-dd_HH-mm-ss}.csv"), result);
 
-                AnsiConsole.MarkupLine("Все ссылки обработаны".MarkupAqua());
-                
-                await exporter.Export(Path.Join(results, $"VK_{DateTime.Now:yyyy-MM-dd_HH-mm-ss}.csv"), result);
-            }
-            else throw new ApplicationException("parser not found");
+            AnsiConsole.MarkupLine("Операция завершена".MarkupAqua());
         }
-        
-        AnsiConsole.MarkupLine("Операция завершена".MarkupAqua());
+        else
+        {
+            AnsiConsole.MarkupLine("Нет данных для сохранения".MarkupRed());
+        }
     }
 
     public async Task Authorization()
     {
         AnsiConsole.MarkupLine("Пройдите авторизацию и нажмите любую клавишу...".MarkupAqua());
-        
+
         using var drv = await ChrDrvFactory.Create(drvSettings);
 
         Console.ReadKey(true);
